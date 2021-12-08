2fdcfb029b8d7df64348821dfa71baa8fde2bd41
Peck Timothy <timothy.peck@he-arc.ch>
2021-12-08 12:45:39 +0100

    Add flashing to battery light in locker in op room

---
+++ rattus_game/Assets/Scipts/op_room_scripts/cabinet_light_flasher.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class cabinet_light_flasher : MonoBehaviour
+{
+
+    public float totalSeconds;     // The total of seconds the flash wil last
+    public float maxIntensity;     // The maximum intensity the flash will reach
+    public Light myLight;        // Your light
+    private int cpt=1;
+    private bool increase = true;
+    private float flashTime;
+
+    void Start()
+    {
+        flashTime = totalSeconds / Time.deltaTime;
+    }
+    void Update()
+    {
+        if (increase)
+        {
+            cpt++;
+            increase = cpt < flashTime ? true : false;
+            myLight.intensity = (maxIntensity * cpt) / flashTime;
+        }
+        else
+        {
+            cpt--;
+            increase = cpt <= 0 ? true : false;
+            myLight.intensity = (maxIntensity * cpt) / flashTime;
+        }
+    }
+}
