464ccc9a3137fc27558a48f4c73c79905d4310f5
Dorian Tan <doriantan7@gmail.com>
2022-01-23 18:02:55 +0100

    try put sound

--- rattus_game/Assets/Scipts/office_scripts/office_verification.cs
+++ rattus_game/Assets/Scipts/office_scripts/office_verification.cs
@@ -7,6 +7,7 @@ namespace Rattus
     {
         private Dictionary<string, bool> Conditions = new Dictionary<string, bool>();
         public Dialogue dialogue;
+        public AudioSource scream;
 
         // Start is called before the first frame update
         void Start()
@@ -25,17 +26,29 @@ namespace Rattus
                 {
                     Conditions["ReadLetter"] = true;
                     dialogue.AddSentence("Me", "A letter, with my name on it.");
-                    dialogue.AddSentence("Me", "It reads:", 1);
-                    dialogue.AddSentence("Letter", "Hello John,\nMy name is Fred Taylor, husband of Josephine Taylor.", 4);
-                    dialogue.AddSentence("Letter", "She was a patient here many years, while you were still the Chief of Medicine", 5);
+                    dialogue.AddSentence("Me", "It reads:", 2);
+                    dialogue.AddSentence("Letter", "Hello John,\nMy name is Fred Taylor,", 4);
+                    dialogue.AddSentence("Letter", "husband of Josephine Taylor.", 4);
+                    dialogue.AddSentence("Letter", "She was a patient here many years,", 5);
+                    dialogue.AddSentence("Letter", "while you were still the Chief of Medicine", 5);
                     dialogue.AddSentence("Me", "So that's how I know those things.", 3);
-                    dialogue.AddSentence("Letter", "You greenlit a new procedure on her, said it would help, you said there was very little risk invovled", 6);
-                    dialogue.AddSentence("Letter", "What you never said was the name of the procedure, which I later found from one of the nurses who went throught the same ordeal as you, was that is was a transcranial lobotomy.", 8);
-                    dialogue.AddSentence("Letter", "But that wasn't the only procedure you did to her that day was it, you also performed a hemispherectomy, against our will.", 7);
-                    dialogue.AddSentence("Letter", "So whatever was left of her after the lobotomy was removed with half of her brain.", 4);
+                    dialogue.AddSentence("Letter", "You greenlit a new procedure on her,", 6);
+                    dialogue.AddSentence("Letter", "you said it would help,", 6);
+                    dialogue.AddSentence("Letter", "you said there was very little risk invovled.", 6);
+                    dialogue.AddSentence("Letter", "What you never said was the name of the procedure,", 8);
+                    dialogue.AddSentence("Letter", "which I later found from one of the nurses", 8);
+                    dialogue.AddSentence("Letter", "who went throught the same ordeal as you,", 8);
+                    dialogue.AddSentence("Letter", "was that is was a transcranial lobotomy.", 8);
+                    dialogue.AddSentence("Letter", "But that wasn't the only procedure", 7);
+                    dialogue.AddSentence("Letter", "you did to her that day was it,", 7);
+                    dialogue.AddSentence("Letter", "you also performed a hemispherectomy,", 7);
+                    dialogue.AddSentence("Letter", "against our will.", 7);
+                    dialogue.AddSentence("Letter", "So whatever was left of her after the lobotomy", 4);
+                    dialogue.AddSentence("Letter", "was removed with half of her brain.", 4);
                     dialogue.AddSentence("Letter", "She became a \"zombie\" as they say in the films.", 4);
-                    dialogue.AddSentence("Letter", "So I contacted some people. Paid some money, and brought her back.", 4);
-                    dialogue.AddSentence("", "*footsteps*", 2);
+                    dialogue.AddSentence("Letter", "So I contacted some people.", 4);
+                    dialogue.AddSentence("Letter", "Paid some money, and brought her back.", 4);
+                    dialogue.AddSentence("", "*Door sound*\n*footsteps*", 2);
                     dialogue.AddSentence("Letter", "And she wants to take her revenge.", 3);
                     dialogue.AddSentence("", "...", 4);
                     FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
@@ -43,8 +56,8 @@ namespace Rattus
                     RenderSettings.skybox.SetColor("_SkyTint", Color.black);
                     GameObject.Find("btnLeft").SetActive(false);
                     GameObject.Find("btnRight").SetActive(false);
+                    scream.Play(); //marche pas 
                 }
-
                 clickableObj.resetLastClicked();
             }
         }
